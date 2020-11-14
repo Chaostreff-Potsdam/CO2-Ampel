3bc9a4bc46955ab4286d6c474adc68af3bc8ff76
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2020-11-14 14:57:43 +0100

    JSON Leerzeichen

--- co2-ampel_lua/app_mhz19.lua
+++ co2-ampel_lua/app_mhz19.lua
@@ -32,7 +32,7 @@ end
 
 local function mhz19_calculate_value(highDuration, lowDuration)
     val = config.HW.MZZ19_MAX * (1002.0 * highDuration - 2.0 * lowDuration) / 1000.0 / (highDuration + lowDuration);
-    print("{ \"ppmCO2\":" .. val .. " }")
+    print("{ \"ppmCO2\": " .. val .. " }")
     return val
 end
 
