2263b3d7284ed9f8075fd5b0222a428fd5aeb7ff
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2020-11-15 01:59:44 +0100

    Helligkeit einstellbar

--- co2-ampel_lua/rgbw_module.lua
+++ co2-ampel_lua/rgbw_module.lua
@@ -5,6 +5,7 @@ print("Load RGBW")
 ws2812.init()
 
 local maxled = 15
+local bright = 50
 
 buff = ws2812.newBuffer(maxled, 4)
 buffer_white = ws2812.newBuffer(maxled, 4)
@@ -14,9 +15,9 @@ buffer_red = ws2812.newBuffer(maxled, 4)
 
 buff:fill(0, 0, 0, 10)
 
-buffer_white:fill(0, 0, 0, 255)
-buffer_green:fill(255, 0, 0, 0)
-buffer_red:fill(0, 255, 0, 0)
+buffer_white:fill(0, 0, 0, bright)
+buffer_green:fill(bright, 0, 0, 0)
+buffer_red:fill(0, bright, 0, 0)
 ws2812.write(buff) -- turn the two first RGBW leds to white
 
 function round(a)
@@ -30,8 +31,8 @@ function led_set()
         if co2 < 800 then
             buff:mix(256, buffer_green)
         elseif co2 < 1000 then
-            rd = math.ceil((co2 - 800) / 200 * 255)
-            gn = 255 - rd
+            rd = math.ceil((co2 - 800) / 200 * 256)
+            gn = 256 - rd
             buff:mix(gn, buffer_green, rd, buffer_red)
         else
             buff:mix(256, buffer_red)
@@ -43,15 +44,19 @@ function led_set()
             elseif (i > (co2 / 100)) then
                 ye = co2 % 100 / 100
                 if co2 < 800 then
-                    buff:set(i, {ye*255, 0, 0, 0})
+                    buff:set(i, {ye*bright, 0, 0, 0})
                 elseif co2 < 1000 then
-                    buff:set(i, {ye*gn, ye*rd, 0, 0})
+                    buff:set(i, {ye*gn/256*bright, ye*rd/256*bright, 0, 0})
                 else
-                    buff:set(i, {0, ye*255, 0, 0})
+                    buff:set(i, {0, ye*bright, 0, 0})
                 end
             end
         end
     end
+    if (wifi.sta.status() == 5) then
+        buff:set(1, {0, 0, bright, 0})
+    end
+ --   print(buff)
     ws2812.init()
     ws2812.write(buff) -- turn the two first RGBW leds to white
 
