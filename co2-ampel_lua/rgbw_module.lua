4324438ea3df51cca83ac0dea912b80e5520b1ae
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2020-11-15 01:06:56 +0100

    Farb-Übergang grün-gelb.

--- co2-ampel_lua/rgbw_module.lua
+++ co2-ampel_lua/rgbw_module.lua
@@ -6,53 +6,60 @@ ws2812.init()
 
 local maxled = 15
 
-buffer = ws2812.newBuffer(maxled, 4)
-local buffer_white = ws2812.newBuffer(maxled, 4)
-local buffer_green = ws2812.newBuffer(maxled, 4)
-local buffer_yellow = ws2812.newBuffer(maxled, 4)
-local buffer_red = ws2812.newBuffer(maxled, 4)
+buff = ws2812.newBuffer(maxled, 4)
+buffer_white = ws2812.newBuffer(maxled, 4)
+buffer_green = ws2812.newBuffer(maxled, 4)
+buffer_yellow = ws2812.newBuffer(maxled, 4)
+buffer_red = ws2812.newBuffer(maxled, 4)
 
-buffer:fill(0, 0, 0, 255)
-ws2812.write(buffer) -- turn the two first RGBW leds to white
+buff:fill(0, 0, 0, 10)
 
-buffer_white:fill(0, 0, 0, 127)
+buffer_white:fill(0, 0, 0, 255)
 buffer_green:fill(255, 0, 0, 0)
 buffer_red:fill(0, 255, 0, 0)
-buffer_yellow:mix(127, buffer_green, 127, buffer_red)
+ws2812.write(buff) -- turn the two first RGBW leds to white
 
 function round(a)
     return 
 end
 
 function led_set()
+    local rd = 0
+    local gn = 0
     if (type(co2) == "number") then
         if co2 < 800 then
-            buffer = buffer_green
+            buff:mix(256, buffer_green)
         elseif co2 < 1000 then
-            local rd = math.ceil((co2 - 800) / 200 * 255)
-            local gn = 255 - rd
-            print("rd:"..rd.."-gn:"..gn)
-            buffer:mix(gn, buffer_green, rd, buffer_red)
+            rd = math.ceil((co2 - 800) / 200 * 255)
+            gn = 255 - rd
+            buff:mix(gn, buffer_green, rd, buffer_red)
         else
-            buffer = buffer_red
+            buff:mix(256, buffer_red)
         end
         
         for i = maxled,1,-1 do
-            if ((co2 / 100) < i) then
-                buffer:set(i, {0,0,0,0})
+            if (i-1 > (co2 / 100)) then
+                buff:set(i, {0,0,0,0})
+            elseif (i > (co2 / 100)) then
+                ye = co2 % 100 / 100
+                if co2 < 800 then
+                    buff:set(i, {ye*255, 0, 0, 0})
+                elseif co2 < 1000 then
+                    buff:set(i, {ye*gn, ye*rd, 0, 0})
+                else
+                    buff:set(i, {0, ye*255, 0, 0})
+                end
             end
         end
-    print("led_set: " .. co2)
     end
-    
     ws2812.init()
-    ws2812.write(buffer) -- turn the two first RGBW leds to white
+    ws2812.write(buff) -- turn the two first RGBW leds to white
 
 end
 
 function led_start()
     ledtmr = tmr.create()
-    ledtmr:register(5000, tmr.ALARM_AUTO, led_set)
+    ledtmr:register(1000, tmr.ALARM_AUTO, led_set)
     ledtmr:start()
 end
 
